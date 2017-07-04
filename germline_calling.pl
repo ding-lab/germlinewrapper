bf3656edfeb7e431f20ccd701f0a07dbebc7d45d
scao <scao@genome.wustl.edu>
2017-07-03 23:57:31 -0500

    add pindel

--- germline_calling.pl
+++ germline_calling.pl
@@ -98,12 +98,14 @@ if ($step_number < 5) {
                    &bsub_gatk();
                    &bsub_varscan();
                    &bsub_pindel();
-                   &bsub_vep();
+                   #&bsub_vep();
                 }
                  elsif ($step_number == 1) {
                     &bsub_gatk();
                 } elsif ($step_number == 2) {
                     &bsub_varscan(1);
+                }elsif ($step_number == 3) {
+                    &bsub_pindel(1);
                 }
 				}
 				}
@@ -193,7 +195,7 @@ sub bsub_varscan{
         $hold_job_file = $current_job_file;
     }
 
-    $current_job_file = "j2_varscan_".$sample_name.".sh";
+    $current_job_file = "j2_varscan_g_".$sample_name.".sh";
     my $IN_bam_T = $sample_full_path."/".$sample_name.".T.bam";
     my $IN_bam_N = $sample_full_path."/".$sample_name.".N.bam";
     if (! -e $IN_bam_T) {#make sure there is a input fasta file 
@@ -271,4 +273,41 @@ sub bsub_varscan{
     $bsub_com = "bsub < $job_files_dir/$current_job_file\n";
     system ( $bsub_com );
 
-	} 
+	}
+
+sub bsub_pindel{
+    my ($step_by_step) = @_;
+    if ($step_by_step) {
+        $hold_job_file = "";
+    }else{
+        $hold_job_file = $current_job_file;
+    }
+
+    $current_job_file = "j3_pindel_g_".$sample_name.".sh";
+    my $IN_bam_T = $sample_full_path."/".$sample_name.".T.bam";
+    my $IN_bam_N = $sample_full_path."/".$sample_name.".N.bam";
+    open(PINDEL, ">$job_files_dir/$current_job_file") or die $!;
+    print PINDEL "#!/bin/bash\n";
+    print PINDEL "#BSUB -n 4\n";
+    print PINDEL "#BSUB -R \"span[hosts=1] rusage[mem=30000]\"","\n";
+    print PINDEL "#BSUB -M 30000000\n";
+    print PINDEL "#BSUB -o $lsf_file_dir","/","$current_job_file.out\n";
+    print PINDEL "#BSUB -e $lsf_file_dir","/","$current_job_file.err\n";
+    print PINDEL "#BSUB -J $current_job_file\n";
+    print PINDEL "#BSUB -w \"$hold_job_file\"","\n";
+    print PINDEL "TBAM=".$sample_full_path."/".$sample_name.".T.bam\n";
+    print PINDEL "NBAM=".$sample_full_path."/".$sample_name.".N.bam\n";
+    print PINDEL "myRUNDIR=".$sample_full_path."/pindel\n";
+    print PINDEL "CONFIG=\${myRUNDIR}"."/".$sample_name.".config\n";
+    print PINDEL "if [ ! -d \${myRUNDIR} ]\n";
+    print PINDEL "then\n";
+    print PINDEL "mkdir \${myRUNDIR}\n";
+    print PINDEL "fi\n";
+    #print PINDEL "echo \"$IN_bam_T\t500\t$sample_name.T\" > \${CONFIG}\n";
+    print PINDEL "echo \"$IN_bam_N\t500\t$sample_name.N\" >> \${CONFIG}\n";
+    print PINDEL "$pindel -T 4 -f $h37_REF -i \${CONFIG} -o \${myRUNDIR}"."/$sample_name"." -m 6 -w 1 -J $f_centromere\n";
+    close PINDEL;
+    $bsub_com = "bsub < $job_files_dir/$current_job_file\n";
+    system ( $bsub_com );
+    }
+ 
