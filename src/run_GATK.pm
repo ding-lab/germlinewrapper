680e9ac20c03a7add566134e0f073e8585cf3020
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-01-03 17:24:13 -0600

    Preliminary rewrite of run_GATK.pm
    * not yet executing

--- src/run_GATK.pm
+++ src/run_GATK.pm
@@ -1,93 +1,96 @@
 
+# Confirm input normal BAM file exists and is not empty
 sub bsub_gatk{
-    #my $cdhitReport = $sample_full_path."/".$sample_name.".fa.cdhitReport";
+    my $NBAM = shift;
+    my $sample_name = shift;
+    my $sample_full_path = shift;
+    my $job_files_dir = shift;
+    my $bsub = shift;
+    my $REF = shift;
+    my $gatk = shift;
+    my $picard = shift;
+    my $status_rg = shift;
+
+
+
     $current_job_file = "j1_gatk_g_".$sample_name.".sh";
-    my $lsf_out=$lsf_file_dir."/".$current_job_file.".out";
-    my $lsf_err=$lsf_file_dir."/".$current_job_file.".err";
-    if(-e $lsf_out) {
-        `rm $lsf_out`;
-        `rm $lsf_err`;
-        `rm $current_job_file`;
-    }
-    my $IN_bam_T = $sample_full_path."/".$sample_name.".T.bam";
-    my $IN_bam_N = $sample_full_path."/".$sample_name.".N.bam";
-	my $IN_bam_N_rg=$sample_full_path."/".$sample_name.".N.rg.bam";
-    #if (! -e $IN_bam_T) {#make sure there is a input fasta file 
-    #    print $red,  "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
-    #    print "Warning: Died because there is no input bam file for bwa:\n";
-    #    print "File $IN_bam_T does not exist!\n";
-    #    die "Please check command line argument!", $normal, "\n\n";
+    my $outfn = "$job_files_dir/$current_job_file";
+    print("Writing to $outfn\n");
 
-    #}
-   # if (! -s $IN_bam_T) {#make sure input fasta file is not empty
-    #    print $red, "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
-    #    die "Warning: Died because $IN_bam_T is empty!", $normal, "\n\n";
-    #}
-    if (! -e $IN_bam_N) {#make sure there is a input fasta file 
-        print $red,  "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
-        print "Warning: Died because there is no input bam file for bwa:\n";
-        print "File $IN_bam_N does not exist!\n";
-        die "Please check command line argument!", $normal, "\n\n";
 
+    my $workdir="$sample_full_path/gatk";
+    system("mkdir -p $workdir");
+
+    # Intermediate and output files
+    # rg files are generated if status_rg = 0.  They are then removed
+    # Their names are created by replacing trailing .bam of $NBAM with .rg.bam
+    $NBAM_rg="${NBAM%.bam}.rg.bam";
+
+    # rawvcf is output of Haplotype Caller step.
+    $rawvcf="$workdir/$sample_name.raw.vcf";
+    # rawvcf is labeled and copied to gvipvcf
+    $gvipvcf="$workdir/$sample_name.gvip.vcf";
+
+    # These two below are outputs
+    $snvvcf="$workdir/$sample_name.snv.gvip.vcf";
+    $indelvcf="$workdir/$sample_name.indel.gvip.vcf";
+
+    # Step 1 is GATK HaplotypeCaller.  Optionally, call `picard AddOrReplaceReadGroups` based on value of status_rg
+    # Input: $NBAM
+    # Output: $rawvcf
+    # if doing AddOrReplaceReadGroups,
+    #   * Create NBAM_rg
+    #   * Index NBAM_rg with samtools
+    #   * run haplotype caller on NBAM_rg
+    #   * Delete NBAM_rg and the index file NBAM_rg.bai
+
+    # Step 2 labels the output from step 1 with GenomeVIP-specific annotation
+
+    # Step 3 pulls out SNV and indel variants
+
+    # Args to picard AddOrReplaceReadGroups.  This should be specified elsewhere
+    my $picard_args = "RGID=1 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=20";
+
+    # Args to HaplotypeCaller.  Thsi should be specified elsewhere
+    my $haplotype_args = "-mbq 10 -rf DuplicateRead -rf UnmappedRead -stand_call_conf 10.0";
+
+    my $step1;
+    if ($status_rg) { # <srg> = bam having read group or not: 1, yes and 0, no (default 1)
+        $step1 = <<'EOF'; 
+            java  \${JAVA_OPTS} -jar $gatk -R $REF  -T HaplotypeCaller -I \${NBAM} -o \${rawvcf} $haplotype_args
+EOF
+    } else {
+        $step1 = <<'EOF'; 
+            java  \${JAVA_OPTS} -jar $picard AddOrReplaceReadGroups I=\${NBAM} O=\${NBAM_rg} $picard_args
+            samtools index \${NBAM_rg}
+            java  \${JAVA_OPTS} -jar $gatk -R $REF  -T HaplotypeCaller -I \${NBAM_rg} -o \${rawvcf} $haplotype_args
+            rm \${NBAM_rg}
+            rm \${NBAM_rg}.bai  # created by `samtools index`
+EOF
     }
-    if (! -s $IN_bam_N) {#make sure input fasta file is not empty
-        print $red, "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
-        die "Warning: Died because $IN_bam_N is empty!", $normal, "\n\n";
-    }
-    open(GATK, ">$job_files_dir/$current_job_file") or die $!;
-    print GATK "#!/bin/bash\n";
-    print GATK "#BSUB -n 1\n";
-    print GATK "#BSUB -R \"rusage[mem=30000]\"","\n";
-    print GATK "#BSUB -M 30000000\n";
-    print GATK "#BSUB -o $lsf_file_dir","/","$current_job_file.out\n";
-    print GATK "#BSUB -e $lsf_file_dir","/","$current_job_file.err\n";
-    print GATK "#BSUB -J $current_job_file\n";
-    print GATK "TBAM=".$sample_full_path."/".$sample_name.".T.bam\n";
-    print GATK "NBAM=".$sample_full_path."/".$sample_name.".N.bam\n";
-  	print GATK "NBAM_rg=".$sample_full_path."/".$sample_name.".N.rg.bam\n";
-	print GATK "NBAM_rg_bai=".$sample_full_path."/".$sample_name.".N.rg.bam.bai\n";
-	print GATK "myRUNDIR=".$sample_full_path."/gatk\n";
-    print GATK "rawvcf=".$sample_full_path."/gatk/".$sample_name.".raw.vcf\n";
-	print GATK "gvipvcf=".$sample_full_path."/gatk/".$sample_name.".gvip.vcf\n";
-	print GATK "snvvcf=".$sample_full_path."/gatk/".$sample_name.".snv.gvip.vcf\n";
-	print GATK "indelvcf=".$sample_full_path."/gatk/".$sample_name.".indel.gvip.vcf\n";
-    print GATK "RUNDIR=".$sample_full_path."\n";
-    print GATK "CONFDIR="."/gscmnt/gc2521/dinglab/cptac_prospective_samples/exome/config\n";
-    print GATK "export SAMTOOLS_DIR=/gscmnt/gc2525/dinglab/rmashl/Software/bin/samtools/1.2/bin\n";
-    print GATK "export JAVA_HOME=$java_dir\n";
-    print GATK "export JAVA_OPTS=\"-Xms256m -Xmx512m\"\n";
-    print GATK "export PATH=\${JAVA_HOME}/bin:\${PATH}\n";
-    print GATK "if [ ! -d \${myRUNDIR} ]\n";
-    print GATK "then\n";
-    print GATK "mkdir \${myRUNDIR}\n";
-    print GATK "fi\n";
-    print GATK "if \[\[ -z \"\$LD_LIBRARY_PATH\" \]\] \; then\n";
-    print GATK "export LD_LIBRARY_PATH=\${JAVA_HOME}/lib\n";
-    print GATK "else\n";
-    print GATK "export LD_LIBRARY_PATH=\${JAVA_HOME}/lib:\${LD_LIBRARY_PATH}\n";
-    print GATK "fi\n";
-    print GATK "BAMLIST=\${RUNDIR}/gatk/bamfilelist.inp\n";
-    print GATK "if [ ! -e \${BAMLIST} ]\n";
-    print GATK "then\n";
-    print GATK "rm \${BAMLIST}\n";
-    print GATK "fi\n";
-   # print GATK "echo \"$IN_bam_N_rg\" > \${BAMLIST}\n";
-    print GATK "if [ $status_rg -eq 0 ]\n";
-	print GATK "then\n";
-	print GATK "java  \${JAVA_OPTS} -jar "."$picardexe AddOrReplaceReadGroups I=\${NBAM} O=\${NBAM_rg} RGID=1 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=20\n";
-	print GATK "samtools index \${NBAM_rg}\n";
-   # print GATK "else\n";
-	print GATK "java  \${JAVA_OPTS} -jar "."$gatkexe -R $h37_REF"."  -T HaplotypeCaller -I \${NBAM_rg} -mbq  10  -rf DuplicateRead  -rf UnmappedRead  -stand_call_conf 10.0  -o  \${rawvcf}\n";
-    print GATK "rm \${NBAM_rg}\n";
-    print GATK "rm \${NBAM_rg_bai}\n";
-	print GATK "else\n";
-	print GATK "java  \${JAVA_OPTS} -jar "."$gatkexe -R $h37_REF"."  -T HaplotypeCaller -I \${NBAM} -mbq  10  -rf DuplicateRead  -rf UnmappedRead  -stand_call_conf 10.0  -o  \${rawvcf}\n";
-    print GATK "fi\n";
-	print GATK "     ".$run_script_path."genomevip_label.pl GATK \${rawvcf} \${gvipvcf}"."\n";
-	print GATK "java \${JAVA_OPTS} -jar "."$gatkexe -R $h37_REF"." -T SelectVariants  -V  \${gvipvcf}  -o  \${snvvcf}  -selectType SNP -selectType MNP"."\n";
-	print GATK "java \${JAVA_OPTS} -jar "."$gatkexe -R $h37_REF"." -T SelectVariants  -V  \${gvipvcf}   -o  \${indelvcf}  -selectType INDEL"."\n";	
-	close GATK;
-    $bsub_com = "bsub < $job_files_dir/$current_job_file\n";
+
+    open(OUT, ">$outfn") or die $!;
+    print OUT <<"EOF";
+        #!/bin/bash
+        export JAVA_OPTS=\"-Xms256m -Xmx512m\"
+
+        # Step 1
+        $step1
+
+        # Step 2
+        $run_script_path/genomevip_label.pl GATK \${rawvcf} \${gvipvcf}
+
+        # Step 3
+        java \${JAVA_OPTS} -jar $gatk -R $REF -T SelectVariants  -V  $gvipvcf  -o $snvvcf  -selectType SNP -selectType MNP"."
+        java \${JAVA_OPTS} -jar $gatk -R $REF -T SelectVariants  -V  $gvipvcf  -o $indelvcf  -selectType INDEL"."
+
+        echo Written final result to $snvvcf and $indelvcf
+EOF
+
+    close OUT;
+
+    $bsub_com = "$bsub < $job_files_dir/$current_job_file\n";
+    print("Executing:\n $bsub_com \n");
     system ( $bsub_com );
 
 	}
